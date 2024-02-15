78a73f47a810770f09cc34ceed543dca91634971
wkjy <u.hikaru0830@gmail.com>
2024-02-15 20:56:42 +0900

    2024/02/15
    2-16

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import { getProviders } from "next-auth/react";
+
 // eslint-disable-next-line @next/next/no-async-client-component
 async function Login() {
+  const providers = await getProviders().then((res) => {
+    console.log(res);
+    return res;
+  });
+
   return (
     <div className="flex items-center justify-center py-16 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
