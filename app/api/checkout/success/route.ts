53db4b901c3028d14f327bff1073a9f078f2878c
wkjy <u.hikaru0830@gmail.com>
2024-02-20 22:20:21 +0900

    2024/02/20
    4-33

--- app/api/checkout/success/route.ts
+++ app/api/checkout/success/route.ts
@@ -11,14 +11,24 @@ export async function POST(request: Request, response: Response) {
   try {
     const session = await stripe.checkout.sessions.retrieve(sessionId);
 
-    const purchase = await prisma.purchase.create({
-      data: {
+    const existingPurchase = await prisma.purchase.findFirst({
+      where: {
         userId: session.client_reference_id!,
         bookId: session.metadata?.bookId!,
       },
     });
 
-    return NextResponse.json({ purchase });
+    if (!existingPurchase) {
+      const purchase = await prisma.purchase.create({
+        data: {
+          userId: session.client_reference_id!,
+          bookId: session.metadata?.bookId!,
+        },
+      });
+      return NextResponse.json({ purchase });
+    } else {
+      return NextResponse.json({ message: "すでに購入済みです。" });
+    }
   } catch (err) {
     return NextResponse.json(err);
   }
