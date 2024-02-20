1a35591f37433a43f36fc6861ae7074dab455657
wkjy <u.hikaru0830@gmail.com>
2024-02-20 18:32:19 +0900

    2024/02/20
    4-32

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -4,12 +4,16 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(request: Request, response: Response) {
-  const { title, price } = await request.json();
+  const { title, price, bookId, userId } = await request.json();
   console.log(title, price);
 
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
+      metadata: {
+        bookId: bookId,
+      },
+      client_reference_id: userId,
       line_items: [
         {
           price_data: {
--- app/api/checkout/success/route.ts
+++ app/api/checkout/success/route.ts
@@ -1,8 +1,24 @@
+import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 //購入履歴の保存
 export async function POST(request: Request, response: Response) {
+  const { sessionId } = await request.json();
+
   try {
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+    const purchase = await prisma.purchase.create({
+      data: {
+        userId: session.client_reference_id!,
+        bookId: session.metadata?.bookId!,
+      },
+    });
+
+    return NextResponse.json({ purchase });
   } catch (err) {
     return NextResponse.json(err);
   }
