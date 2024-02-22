dddd2f56aa04e7e058653364ee45c702d2b2074f
wkjy <u.hikaru0830@gmail.com>
2024-02-22 20:30:06 +0900

    2024/02/22
    4-40

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -10,22 +10,26 @@ const DetailBook = async ({ params }: { params: { id: string } }) => {
     <div className="container mx-auto p-4">
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
         <Image
-          src={""}
+          src={book.thumbnail.url}
           className="w-full h-80 object-cover object-center"
           width={700}
           height={700}
-          alt="book image"
+          alt={book.title}
         />
         <div className="p-4">
-          <h2 className="text-2xl font-bold">Test Book</h2>
+          <h2 className="text-2xl font-bold">{book.title}</h2>
           <div
             className="text-gray-700 mt-2"
-            dangerouslySetInnerHTML={{ __html: "<h1>Test</h1>" }}
+            dangerouslySetInnerHTML={{ __html: book.content }}
           />
 
           <div className="flex justify-between items-center mt-2">
-            <span className="text-sm text-gray-500">公開日:</span>
-            <span className="text-sm text-gray-500">最終更新:</span>
+            <span className="text-sm text-gray-500">
+              公開日:{new Date(book.createdAt as any).toLocaleString()}
+            </span>
+            <span className="text-sm text-gray-500">
+              最終更新:{new Date(book.updatedAt).toLocaleString()}
+            </span>
           </div>
         </div>
       </div>
