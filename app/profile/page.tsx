020a159bc17e88a0363cee5c73b75e22226fef6f
wkjy <u.hikaru0830@gmail.com>
2024-02-24 19:50:53 +0900

    2024/02/24
    5-45

---
+++ app/profile/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "../lib/next-auth/options";
+import { User } from "../types/types";
+
+export default async function ProfilePage() {
+  const session = await getServerSession(nextAuthOptions);
+  const user = session?.user as User;
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-xl font-bold mb-4">プロフィール</h1>
+
+      <div className="bg-white shadow-md rounded p-4">
+        <div className="flex items-center">
+          <Image
+            priority
+            src={user.image || "/default_icon.png"}
+            alt="user profile_icon"
+            width={60}
+            height={60}
+            className="rounded-t-md"
+          />
+          <h2 className="text-lg ml-4 font-semibold">お名前：{user.name}</h2>
+        </div>
+      </div>
+
+      <span className="font-medium text-lg mb-4 mt-4 block">購入した記事</span>
+      <div className="flex items-center gap-6"></div>
+    </div>
+  );
+}
