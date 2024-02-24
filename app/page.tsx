e133edc871acf7a73dac2c7381bf02065fff642b
wkjy <u.hikaru0830@gmail.com>
2024-02-24 19:42:03 +0900

    2024/02/24
    4-44

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@
 import { getServerSession } from "next-auth";
 import Book from "./components/Book";
 import { getAllBooks } from "./lib/microcms/client";
-import { BookType } from "./types/types";
+import { BookType, Purchase, User } from "./types/types";
 import { nextAuthOptions } from "./lib/next-auth/options";
 
 // eslint-disable-next-line @next/next/no-async-client-component
 export default async function Home() {
   const { contents } = await getAllBooks();
   const session = await getServerSession(nextAuthOptions);
-  const user: any = session?.user;
+  const user = session?.user as User;
 
-  let purchaseBookIds: any;
+  let purchaseBookIds: string[];
 
   if (user) {
     const response = await fetch(
@@ -23,7 +23,7 @@ export default async function Home() {
     //console.log(purchasesData);
 
     purchaseBookIds = purchasesData.map(
-      (purchaseBook: any) => purchaseBook.bookId
+      (purchaseBook: Purchase) => purchaseBook.bookId
     );
     //console.log(purchaseBookIds);
   }
