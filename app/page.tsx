78ae8d4180d14d1f7901714a8bf5e9593de26a79
wkjy <u.hikaru0830@gmail.com>
2024-02-26 16:34:14 +0900

    2024/02/26
    TypeError: Cannot read properties of undefined (reading 'includes')
    
    error fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
   const session = await getServerSession(nextAuthOptions);
   const user = session?.user as User;
 
-  let purchaseBookIds: string[];
+  let purchaseBookIds: string[] = [];
 
   if (user) {
     const response = await fetch(
