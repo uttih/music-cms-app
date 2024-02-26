4c2115d2a50c51522bac8ad9ef33c0ba43f5ef60
wkjy <u.hikaru0830@gmail.com>
2024-02-26 17:59:35 +0900

    2024/02/26
    6-50

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Header from "./components/Header";
 import { SessionProvider } from "next-auth/react";
 import { NextAuthProvider } from "./lib/next-auth/provider";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 const notoSansJP = Noto_Sans_JP({ subsets: ["latin"], weight: ["400"] });
 
@@ -22,7 +24,7 @@ export default function RootLayout({
       <body className={notoSansJP.className}>
         <NextAuthProvider>
           <Header />
-          {children}
+          <Suspense fallback={<Loading />}>{children}</Suspense>
         </NextAuthProvider>
       </body>
     </html>
