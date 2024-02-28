047144fbe3b6f8fb3e4b2d25420c9c7094cfdf54
wkjy <u.hikaru0830@gmail.com>
2024-02-28 17:11:25 +0900

    2024/02/28
    Image

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Book from "./components/Book";
 import { getAllBooks } from "./lib/microcms/client";
 import { BookType, Purchase, User } from "./types/types";
 import { nextAuthOptions } from "./lib/next-auth/options";
+import Image from "next/image";
 
 // eslint-disable-next-line @next/next/no-async-client-component
 export default async function Home() {
@@ -30,10 +31,21 @@ export default async function Home() {
 
   return (
     <>
-      <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
-        <h2 className="text-center w-full font-bold text-3xl mb-2">
-          Book Commerce
-        </h2>
+      <div className="relative w-full md:h-96">
+        <Image
+          src="/Music2.jpg"
+          alt="Header Image"
+          layout="fill"
+          objectFit="cover"
+        />
+        <div className="absolute top-1/3 left-0 transform -translate-y-1/2 p-4">
+          <h1 className="text-white text-4xl md:text-5xl font-bold font-serif">
+            - No Music No Life -
+          </h1>
+        </div>
+      </div>
+      <main className="flex flex-wrap justify-center items-center md:mt-16 mt-15">
+        <h2 className="text-center w-full font-bold text-3xl mb-2">音楽一覧</h2>
         {contents.map((book: BookType) => (
           <Book
             key={book.id}
