e547dbfaeb0314d612faf117df7c741a6e737313
wkjy <u.hikaru0830@gmail.com>
2024-03-01 00:55:08 +0900

    2024/03/01
    force-cache

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
   if (user) {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/purchases/${user.id}`,
-      { cache: "no-store" } //SSR
+      { cache: "force-cache" } //SSR
     );
     const purchasesData = await response.json();
     //console.log(purchasesData);
