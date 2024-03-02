906a2535cd91d824ae59e854e78895baab64a1c5
wkjy <u.hikaru0830@gmail.com>
2024-03-02 20:09:17 +0900

    2024/03/02

--- app/book/checkout-success/page.tsx
+++ app/book/checkout-success/page.tsx
@@ -50,7 +50,7 @@ const PurchaseSuccess = () => {
             href={`/book/${bookUrl}`}
             className="text-indigo-600 hover:text-indigo-800 transition duration-300"
           >
-            購入した記事を読む
+            購入した商品の詳細を見る
           </Link>
         </div>
       </div>
